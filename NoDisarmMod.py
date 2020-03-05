e086a034582eba7768e832b8404b7364ae6f8e84
Tetragromaton <36923729+Tetragromaton@users.noreply.github.com>
2020-03-05 18:18:11 +0700

    Update to recent game version.

--- NoDisarmMod.py
+++ NoDisarmMod.py
@@ -4,13 +4,13 @@ from memory.manager import TypeManager
 
 
 func = find_binary('server.so')[b'']
-#For Linux, version 1.37.4.3 CSGO
-OffsetNudge = 0x00C44E85
+#For Linux, version 1.37.4.4 CSGO
+OffsetNudge = 0x00C44F55
 (func + OffsetNudge).unprotect(8)
 val = func.get_ulong_long(OffsetNudge)
 print("Pre patch value is " + str(val))
-if val != 12747720220099249167:
+if val != 16206484733919790095:
     print("Punch disarm offset not found. Unable to load or this instruction already patched.")
 else:
-    val = func.set_ulong_long(12747598174457632233, OffsetNudge)
+    val = func.set_ulong_long(16206362688278173161, OffsetNudge)
     print("Successfully patched bytes. Enjoy !")
